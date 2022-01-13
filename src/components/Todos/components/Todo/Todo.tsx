bfa4cb905d6ee7dd4eee9bdc221e6a60ea7f9800
lihachev <lihachev@tochka.com>
2022-01-13 14:08:23 +0300

    refactoring (Todos) dragging style

--- src/components/Todos/components/Todo/Todo.tsx
+++ src/components/Todos/components/Todo/Todo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { RenameForm, TodoSettings, TodoStatusForm } from './components'
 import { DropPosition, R_2, TodoType } from '../../models/index.model'
 import useBehaviorSubject from '../../../../pages/rxjs/components/subject/hooks'
@@ -25,21 +25,42 @@ const Todo: React.FC<Props> = ({ props }) => {
   )
 
   const label = shouldShowLabel && <h2 className="todo__label">{todo.label}</h2>
-  function dragOver(evt: any) {
-    evt.preventDefault()
-    const todoHtmlItem = evt.target
-    const box = todoHtmlItem.getBoundingClientRect()
-    const halfHeight = box.height / 2
-    setDragPos(evt.clientY <= box.y + halfHeight ? DropPosition.before : DropPosition.after)
-    getReplaceableTodo(todo)
-  }
+  const draggingTodoRef = useRef<HTMLLIElement | null>(null)
+  const replaceableTodoRef = useRef<HTMLLIElement | null>(null)
 
+  function handleDragOver(evt: React.DragEvent<HTMLLIElement>) {
+    evt.preventDefault()
+    const replaceableTodoEl = evt.target as HTMLLIElement
+    replaceableTodoRef.current = replaceableTodoEl
+    const box = replaceableTodoEl.getBoundingClientRect()
+    const halfHeight = box.height / 2
+    const dropPos = evt.clientY <= box.y + halfHeight ? DropPosition.before : DropPosition.after
+
+    setDragPos(dropPos)
+    getReplaceableTodo(todo)
+    if (dropPos === DropPosition.before) {
+      replaceableTodoRef.current.classList.add('before')
+    }
+    if (dropPos === DropPosition.after) {
+      replaceableTodoRef.current.classList.add('after')
+    }
+  }
+  function handleDragStart(evt: React.DragEvent<HTMLLIElement>) {
+    const draggingTodoEl = evt.target as HTMLLIElement
+    draggingTodoRef.current = draggingTodoEl
+    draggingTodoEl.classList.add('dragging')
+    dragTodo(todo)
+  }
+  function handleDragEnd() {
+    if (draggingTodoRef.current) draggingTodoRef.current.classList.remove('dragging')
+    changeOrder()
+  }
   return (
     <li
       draggable={true}
-      onDragStart={() => dragTodo(todo)}
-      onDragOver={dragOver}
-      onDragEnd={changeOrder}
+      onDragStart={handleDragStart}
+      onDragOver={handleDragOver}
+      onDragEnd={handleDragEnd}
       className={'todo'}
     >
       {label || <RenameForm props={{ updateTodo, setShouldShowLabel, todo, setShouldShowMenu }} />}
