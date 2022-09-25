10b197835c9e12d4c76812677b8eef7356b3f0e9
Vitaly Likhachev <vitaly99878@gmail.com>
2022-09-25 10:33:39 +0300

    add example bridge

---
+++ src/desing-pattern/bridge/index.ts
@@ -0,0 +1,52 @@
+export interface UIToolkit {
+  drawBorder(): void;
+  drawImage(src: string): void;
+  drawText(text: string): void;
+}
+
+abstract class UIElement {
+  constructor(public toolkit: UIToolkit) {}
+  abstract render(): void;
+}
+
+class TextElement extends UIElement {
+  constructor(public text: string, toolkit: UIToolkit) {
+    super(toolkit);
+  }
+  render(): void {
+    this.toolkit.drawText(this.text);
+  }
+}
+
+class ImageElement extends UIElement {
+  constructor(public src: string, toolkit: UIToolkit) {
+    super(toolkit);
+  }
+  render(): void {
+    this.toolkit.drawImage(this.src);
+  }
+}
+
+class SvgToolkit implements UIToolkit {
+  drawBorder(): void {}
+
+  drawImage(src: string): void {}
+
+  drawText(text: string): void {}
+}
+
+class TextToolkit implements UIToolkit {
+  drawBorder(): void {}
+
+  drawImage(src: string): void {}
+
+  drawText(text: string): void {}
+}
+
+let svgToolkit = new SvgToolkit();
+let textToolkit = new TextToolkit();
+
+let imageElement = new ImageElement('foo.jpg', svgToolkit);
+let textElement = new TextElement('bar', textToolkit);
+imageElement.render();
+textElement.render();
